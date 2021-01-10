f20fcaa4ad113d671d03a48a3eb5335a99978c44
Rajeet Hossain <rhossain192096@bscse.uiu.ac.bd>
2021-01-10 17:34:09 +0600

    OTDT Fourth Commit

---
+++ OTDT/src/Aplication/Search.java
@@ -0,0 +1,241 @@
+package Aplication;
+import Login.*;
+import Employe.*;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Search extends JFrame {
+    JPanel panel1 = new JPanel();
+    JPanel panel2 = new JPanel();
+    JPanel panel3 = new JPanel();
+    JPanel panel4 = new JPanel();
+    JPanel panel5 = new JPanel();
+
+    JLabel label1 = new JLabel("Employe ID");
+    JLabel label2 = new JLabel("Name");
+    JLabel label3 = new JLabel("Bank Account");
+    JLabel label4 = new JLabel("Department");
+    JLabel label5 = new JLabel("Designation");
+    JLabel label6 = new JLabel("Area");
+    JLabel label7 = new JLabel("Mobile No");
+    JLabel label8 = new JLabel("Email");
+    JLabel label9 = new JLabel("Address");
+    JLabel label10 = new JLabel("Total Salary");
+
+    JLabel searchLabel = new JLabel("Employe ID");
+
+    JTextField field1 = new JTextField(22);
+    JTextField field2 = new JTextField(22);
+    JTextField field3 = new JTextField(22);
+    JTextField field4 = new JTextField(22);
+    JTextField field5 = new JTextField(22);
+    JTextField field6 = new JTextField(22);
+    JTextField field7 = new JTextField(22);
+    JTextField field8 = new JTextField(22);
+    JTextField field9 = new JTextField(22);
+    JTextField field10 = new JTextField(22);
+
+    public JTextField searchField = new JTextField(22);
+    JTextField displayField = new JTextField(36);
+
+    JButton button1 = new JButton("Show");
+    JButton button2 = new JButton("New User");
+    JButton button3 = new JButton("Logout");
+    JButton button4 = new JButton("Update Data");
+    JButton button5 = new JButton("Salary Breakdown");
+    JButton button6 = new JButton("Staff List");
+    JButton button7 = new JButton("Salary Disbursement");
+
+    public void showDetails(EmployeData person){
+        field1.setText("");
+        field2.setText("");
+        field3.setText("");
+        field4.setText("");
+        field5.setText("");
+        field6.setText("");
+        field7.setText("");
+        field8.setText("");
+        field9.setText("");
+        field10.setText("");
+        displayField.setText("");
+
+        String searchData = searchField.getText().toUpperCase();
+
+        int flag = 0;
+        if (!searchField.getText().equals("")) {
+            for (EmployeData data : person.recordList) {
+                if (data.getId().equals(searchData)) {
+                    field1.setText(data.getId());
+                    field2.setText(data.getName());
+                    field3.setText(data.getBankAcc());
+                    field4.setText(data.getDepartment());
+                    field5.setText(data.getDesignation());
+                    field6.setText(data.getArea());
+                    field7.setText(data.getMobNumber());
+                    field8.setText(data.getEmail());
+                    field9.setText(data.getAddress());
+                    field10.setText(data.getSalaryT()+"");
+                    flag = 1;
+                }
+            }
+            if (flag == 0) {
+                displayField.setText("Employee not Found");
+            }
+        } else {
+            displayField.setText("Please Enter an Employee ID");
+        }
+    }
+
+
+    public Search() {
+        setTitle("Employee Information");
+        setSize(450, 610);
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        setLayout(new FlowLayout());
+        panel1.setPreferredSize(new Dimension(410, 70));
+        panel1.setBorder(BorderFactory.createTitledBorder("Search"));
+        panel2.setLayout(new GridLayout(10, 2));
+        panel2.setPreferredSize(new Dimension(410, 300));
+        panel2.setBorder(BorderFactory.createTitledBorder("Details"));
+        panel3.setPreferredSize(new Dimension(410, 100));
+        panel3.setBorder(BorderFactory.createTitledBorder("Actions"));
+
+        field1.setEditable(false);
+        field2.setEditable(false);
+        field3.setEditable(false);
+        field4.setEditable(false);
+        field5.setEditable(false);
+        field6.setEditable(false);
+        field7.setEditable(false);
+        field8.setEditable(false);
+        field9.setEditable(false);
+        field10.setEditable(false);
+
+        displayField.setEditable(false);
+
+        EmployeData person = new EmployeData();
+
+        person.employeeList();
+
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                showDetails(person);
+            }
+        });
+
+        button2.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Registration registration = new Registration();
+            }
+        });
+
+        button3.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                Login login = new Login();
+            }
+        });
+
+        button4.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                UpdateData update = new UpdateData();
+                update.searchField.setText(field1.getText());
+                update.showDataU(person, update.positionU);
+                update.displayField.setText("");
+                update.frameid = 0;
+            }
+        });
+
+        button5.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                SalaryBreakdown breakdown = new SalaryBreakdown();
+                breakdown.searchField.setText(field1.getText());
+                breakdown.showSalaryBreakdown(person);
+                breakdown.displayField.setText("");
+                breakdown.frameid = 0;
+            }
+        });
+
+        button6.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                StaffList stafflist = new StaffList();
+                stafflist.showAll(person);
+            }
+        });
+
+        button7.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+                SalaryDisbursement disbursement = new SalaryDisbursement();
+                disbursement.searchField.setText(field1.getText());
+                disbursement.showSalaryDisbursement(person);
+                disbursement.displayField.setText("");
+            }
+        });
+
+        panel1.add(searchLabel);
+        panel1.add(searchField);
+        panel1.add(button1);
+
+        panel2.add(label1);
+        panel2.add(field1);
+        panel2.add(label2);
+        panel2.add(field2);
+        panel2.add(label3);
+        panel2.add(field3);
+        panel2.add(label4);
+        panel2.add(field4);
+        panel2.add(label5);
+        panel2.add(field5);
+        panel2.add(label6);
+        panel2.add(field6);
+        panel2.add(label7);
+        panel2.add(field7);
+        panel2.add(label8);
+        panel2.add(field8);
+        panel2.add(label9);
+        panel2.add(field9);
+        panel2.add(label10);
+        panel2.add(field10);
+
+        panel5.add(displayField);
+
+        panel3.add(button6);
+        panel3.add(button5);
+        panel3.add(button4);
+        panel3.add(button7);
+
+        panel4.add(button3);
+        panel4.add(button2);
+
+        add(panel1);
+        add(panel2);
+        add(panel5);
+        add(panel3);
+        add(panel4);
+        setVisible(true);
+
+    }
+}
+
---
+++ OTDT/src/Employe/EmployeData.java
@@ -0,0 +1,405 @@
+package Employe;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class EmployeData {
+    private String id;
+    private String name;
+    private String bankAcc;
+    private String department;
+    private String designation;
+    private String area;
+    private String mobNumber;
+    private String email;
+    private String address;
+
+    private int salaryB;
+    private int salaryT;
+    private int mobAll;
+    private int medAll;
+    private int expAll;
+    private int rentAll;
+    private int eduAll;
+    private int otherAll;
+    private int Bonus;
+    private int ot;
+    private int dt;
+    private int commission;
+    private int salaryDays;
+    private int salaryHours;
+    private int salaryDisbursement;
+    private int totalAll;
+
+
+    //Set Data
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setMobNumber(String mobNumber) {
+        this.mobNumber = mobNumber;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public void setBankAcc(String bankAcc) {
+        this.bankAcc = bankAcc;
+    }
+
+    public void setDepartment(String department) {
+        this.department = department;
+    }
+
+    public void setDesignation(String designation) {
+        this.designation = designation;
+    }
+
+    public void setArea(String area) {
+        this.area = area;
+    }
+
+    public void setSalaryB(int salaryB) {
+        this.salaryB = salaryB;
+    }
+
+    public void setMobAll(int mobAll) {
+        this.mobAll = mobAll;
+    }
+
+    public void setMedAll(int medAll) {
+        this.medAll = medAll;
+    }
+
+    public void setRentAll(int rentAll) {
+        this.rentAll = rentAll;
+    }
+
+    public void setOtherAll(int otherAll) {
+        this.otherAll = otherAll;
+    }
+
+    public void setBonus(int bonus) {
+        Bonus = bonus;
+    }
+
+    public void setOT(int hours, int days) {
+        ot = (getSalaryHours()*hours) + (getSalaryDays()*days);
+    }
+
+    public void setOT(int ot) {
+        this.ot = ot;
+    }
+
+    public void setDT(int hours, int days, int amount) {
+        dt = (getSalaryHours()*hours) + (getSalaryDays()*days) + amount;
+    }
+
+    public void setDT(int dt) {
+        this.dt = dt;
+    }
+
+    public void setCommission(int commission) {
+        this.commission = commission;
+    }
+
+    public void setEduAll(int eduAll) {
+        this.eduAll = eduAll;
+    }
+
+    public void setExpAll(int expAll) {
+        this.expAll = expAll;
+    }
+
+    public void setSalaryT(int salaryT) {
+        this.salaryT = salaryT;
+    }
+    //
+//    public void setSalaryDays() {
+//        this.salaryDays = salaryB/30;
+//    }
+//
+//    public void setSalaryHours() {
+//        this.salaryHours = salaryDays/24;
+//    }
+//
+    public void setSalaryDisbursement(int salaryDisbursement) {
+        this.salaryDisbursement = salaryDisbursement;
+    }
+
+    public void setTotalAll(int totalAll) {
+        this.totalAll = totalAll;
+    }
+
+    //Get Data
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getBankAcc() {
+        return bankAcc;
+    }
+
+    public String getDepartment() {
+        return department;
+    }
+
+    public String getDesignation() {
+        return designation;
+    }
+
+    public String getArea() {
+        return area;
+    }
+
+    public String getMobNumber() {
+        return mobNumber;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public int getSalaryB() {
+        return salaryB;
+    }
+
+    public int getSalaryT() {
+        salaryT = salaryB + mobAll + medAll + eduAll + rentAll + expAll + otherAll;
+        return salaryT;
+    }
+
+    public int getMobAll() {
+        return mobAll;
+    }
+
+    public int getMedAll() {
+        return medAll;
+    }
+
+    public int getRentAll() {
+        return rentAll;
+    }
+
+    public int getOtherAll() {
+        return otherAll;
+    }
+
+    public int getBonus() {
+        return Bonus;
+    }
+
+    public int getOT() {
+        return ot;
+    }
+
+    public int getDT() {
+        return dt;
+    }
+
+    public int getCommission() {
+        return commission;
+    }
+
+    public int getEduAll() {
+        return eduAll;
+    }
+
+    public int getExpAll() {
+        return expAll;
+    }
+
+    public int getSalaryDays() {
+        salaryDays = salaryB/30;
+        return salaryDays;
+    }
+
+    public int getSalaryHours() {
+        salaryHours = getSalaryDays()/24;
+        return salaryHours;
+    }
+
+    public int getSalaryDisbursement() {
+        salaryDisbursement = (salaryT + ot + Bonus + commission) - dt;
+        return salaryDisbursement;
+    }
+
+    public int getTotalAll() {
+        totalAll = mobAll + medAll + eduAll + expAll + rentAll + otherAll;
+        return totalAll;
+    }
+
+
+    private File data = new File("Data.txt");
+    private File temp = new File("Temp.txt");
+    private File dSdata = new File("Disbursement.csv");
+//    private File Adata = new File("Addition");
+//    private File Ddata = new File("Deduction");
+
+    public File getData() {
+        return data;
+    }
+
+
+    //Write to File
+    void write(File file){
+        try {
+            FileWriter write = new FileWriter(file, true);
+
+            write.append(id + ";" + name + ";" + bankAcc + ";" + department + ";" + designation + ";" + area + ";" + mobNumber + ";" + email + ";" + address + ";" + salaryB + ";" + mobAll + ";" + medAll + ";" + expAll + ";" + eduAll + ";" + rentAll + ";" + otherAll +  ";" + getTotalAll() + ";" + getSalaryT() + ";" + Bonus + ";" + ot + ";" + commission + ";" + dt + ";" + getSalaryDisbursement() + "\n");
+
+            write.close();
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    //New Entry
+    public void saveData() {
+        write(data);
+//        try {
+//            FileWriter write = new FileWriter(data, true);
+//
+//            write.append(id + ";" + name + ";" + bankAcc + ";" + department + ";" + designation + ";" + area + ";" + mobNumber + ";" + email + ";" + address + ";" + salaryB + ";" + mobAll + ";" + medAll + ";" + expAll + ";" + eduAll + ";" + rentAll + ";" + otherAll +  ";" + getTotalAll() + ";" + getSalaryT() + ";" + Bonus + ";" + ot + ";" + commission + ";" + dt + ";" + getSalaryDisbursement() + "\n");
+//
+//            write.close();
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+    }
+
+
+    //Update Data
+    public void newData() {
+        write(temp);
+//        try {
+//            FileWriter newData = new FileWriter(temp, true);
+//
+//            newData.append(id + ";" + name + ";" + bankAcc + ";" + department + ";" + designation + ";" + area + ";" + mobNumber + ";" + email + ";" + address + ";" + salaryB + ";" + mobAll + ";" + medAll + ";" + expAll + ";" + eduAll + ";" + rentAll + ";" + otherAll +  ";" + getTotalAll() + ";" + getSalaryT() + ";" + Bonus +  ";" + ot + ";" + commission + ";" + dt + ";" + getSalaryDisbursement() + "\n");
+//
+//            newData.close();
+//
+//        } catch (IOException e) {
+//            System.out.println(e.getMessage());
+//        }
+    }
+
+    public void updateData() {
+        try {
+            Scanner scn = new Scanner(temp);
+            FileWriter updateData = new FileWriter(data);
+
+            while (scn.hasNextLine()) {
+                updateData.write(scn.nextLine() + "\n");
+            }
+
+            updateData.close();
+            scn.close();
+
+            temp.delete();
+
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    //Disbursement Data
+    public void exportTableData(JTable table){
+        try {
+            FileWriter export = new FileWriter(dSdata, true);
+            for(int i=0; i<table.getColumnCount(); i++) {
+                export.append(table.getModel().getColumnName(i)+",");
+            }
+            export.append("\n");
+            for(int i=0; i<table.getRowCount(); i++){
+                for(int j=0; j<table.getColumnCount(); j++){
+                    export.append(table.getModel().getValueAt(i, j).toString()+",");
+                }
+                export.append("\n");
+            }
+            String dateTime = System.currentTimeMillis()+"";
+            export.close();
+            dSdata.renameTo(new File("C:\\Users\\rajee\\Desktop\\Exported"+dateTime+".xml"));
+            dSdata.delete();
+            JOptionPane.showMessageDialog(null,"Data Exported");
+        }
+        catch (IOException e){
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+    //Employee List
+    public ArrayList<EmployeData> recordList;
+    public void employeeList() {
+        recordList = new ArrayList<EmployeData>();
+        try {
+            Scanner read = new Scanner(data);
+            try {
+                while (read.hasNextLine()) {
+                    String data = read.nextLine();
+                    String[] dataSplit = data.split(";");
+                    EmployeData personData = new EmployeData();
+                    personData.setId(dataSplit[0]);
+                    personData.setName(dataSplit[1]);
+                    personData.setBankAcc(dataSplit[2]);
+                    personData.setDepartment(dataSplit[3]);
+                    personData.setDesignation(dataSplit[4]);
+                    personData.setArea(dataSplit[5]);
+                    personData.setMobNumber(dataSplit[6]);
+                    personData.setEmail(dataSplit[7]);
+                    personData.setAddress(dataSplit[8]);
+                    personData.setSalaryB(Integer.parseInt(dataSplit[9]));
+                    personData.setMobAll(Integer.parseInt(dataSplit[10]));
+                    personData.setMedAll(Integer.parseInt(dataSplit[11]));
+                    personData.setExpAll(Integer.parseInt(dataSplit[12]));
+                    personData.setEduAll(Integer.parseInt(dataSplit[13]));
+                    personData.setRentAll(Integer.parseInt(dataSplit[14]));
+                    personData.setOtherAll(Integer.parseInt(dataSplit[15]));
+                    personData.setTotalAll(Integer.parseInt(dataSplit[16]));
+                    personData.setSalaryT(Integer.parseInt(dataSplit[17]));
+                    personData.setBonus(Integer.parseInt(dataSplit[18]));
+                    personData.setOT(Integer.parseInt(dataSplit[19]));
+                    personData.setCommission(Integer.parseInt(dataSplit[20]));
+                    personData.setDT(Integer.parseInt(dataSplit[21]));
+                    personData.setSalaryDisbursement(Integer.parseInt(dataSplit[22]));
+
+                    recordList.add(personData);
+
+                }
+
+            } catch (Exception f) {
+                System.out.println(f.getMessage());
+            }
+        } catch (FileNotFoundException f) {
+            System.out.println(f.getMessage());
+        }
+    }
+}
