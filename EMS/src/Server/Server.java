56b42111143ee64a7110bf400887f38d86a23816
Rajeet Hossain <rhossain192096@bscse.uiu.ac.bd>
2021-01-28 21:11:59 +0600

    EMS Twelvth Commit

--- EMS/src/Server/Server.java
+++ EMS/src/Server/Server.java
@@ -13,33 +13,39 @@ import java.util.Scanner;
 public class Server extends Thread{
     public void run() {
         EmployeData employeData = new EmployeData();
-
         try {
             ServerSocket server = new ServerSocket(5000);
             System.out.println("Server is running...");
             System.out.println("Waiting for connection...");
 
-            Socket socket = server.accept();
-            System.out.println("Connected...");
+            while (true) {
+                Socket socket = server.accept();
+                System.out.println("Connected...");
 
-            File A = new File("A.txt");
+//                File A = new File("A.txt");
 
-            InputStreamReader isr = new InputStreamReader(socket.getInputStream());
-            Scanner reader = new Scanner(isr);
+                InputStreamReader isr = new InputStreamReader(socket.getInputStream());
+                Scanner reader = new Scanner(isr);
 //            fileName = reader.nextLine();
 //            fileName = fileName + ".txt";
 
 //            System.out.println(fileName);
 
-            FileWriter writer = new FileWriter(A);
+                FileWriter writer = new FileWriter(employeData.otdt);
+//                FileWriter writer = new FileWriter(A);
 
-            while (reader.hasNextLine()) {
-                String line = reader.nextLine();
-                System.out.println(line);
-                writer.append(line + "\n");
-                writer.flush();
+                while (reader.hasNextLine()) {
+                    String line = reader.nextLine();
+                    if(line.equals("<eof>")) {
+                        break;
+                    }
+                    System.out.println(line);
+                    writer.append(line + "\n");
+                    writer.flush();
+                }
+                socket.close();
+                System.out.println("Disconnected...");
             }
-            socket.close();
         } catch (IOException e) {
             System.out.println(e.getMessage());
         }
