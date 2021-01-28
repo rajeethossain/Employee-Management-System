1f1d90263375880e58b2baa74c403691043c0c49
Rajeet Hossain <rhossain192096@bscse.uiu.ac.bd>
2021-01-28 21:10:45 +0600

    Server Second Commit

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -1,42 +1,94 @@
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class Server extends Thread{
-    public void run() {
+public class Server{
+    public static void main(String[] args) {
         try {
             ServerSocket server = new ServerSocket(5000);
             System.out.println("Server is running...");
             System.out.println("Waiting for connection...");
 
+            File otdt = new File("OTDT.txt");
+
             while (true) {
                 Socket socket = server.accept();
                 System.out.println("Connected...");
 
-                File otdt = new File("OTDT.txt");
-
                 InputStreamReader isr = new InputStreamReader(socket.getInputStream());
                 Scanner reader = new Scanner(isr);
-//            fileName = reader.nextLine();
-//            fileName = fileName + ".txt";
 
-//            System.out.println(fileName);
+                OutputStreamWriter osw = new OutputStreamWriter(socket.getOutputStream());
+                PrintWriter writer = new PrintWriter(osw);
 
-                FileWriter writer = new FileWriter(otdt);
+                Scanner fileReader = new Scanner(otdt);
+                FileWriter fileWriter = new FileWriter(otdt);
 
-                while (reader.hasNextLine()) {
-                    String line = reader.nextLine();
-                    if(line.equals("<eof>")) {
-                        break;
+
+//
+                try {
+                    while (reader.hasNextLine()) {
+                        String line = reader.nextLine();
+                        if (line.equals("<eof>")) {
+                            break;
+                        }
+                        System.out.println(line);
+                        fileWriter.append(line + "\n");
+                        fileWriter.flush();
                     }
-                    System.out.println(line);
-                    writer.append(line + "\n");
-                    writer.flush();
                 }
+                catch (Exception e){
+                    System.out.println(e.getMessage());
+                }
+
+//
+//                while (fileReader.hasNextLine()){
+//                    String line = fileReader.nextLine();
+//                    writer.write(line + "\n");
+//                    writer.flush();
+//                }
+//                writer.write("<eof>\n");
+//                writer.flush();
+
+//                Thread receive = new Thread(){
+//                    @Override
+//                    public void run() {
+//                        try {
+//                            while (reader.hasNextLine()) {
+//                                String line = reader.nextLine();
+//                                if (line.equals("<eof>")) {
+//                                    break;
+//                                }
+//                                System.out.println(line);
+//                                fileWriter.append(line + "\n");
+//                                fileWriter.flush();
+//                            }
+//                        }
+//                        catch (Exception e){
+//                            System.out.println(e.getMessage());
+//                        }
+//                    }
+//                };
+//                receive.start();
+//
+//                Thread send = new Thread(){
+//                    @Override
+//                    public void run() {
+//                        while (filereader.hasNextLine()){
+//                            String line = filereader.nextLine();
+//                            writer.write(line + "\n");
+//                            writer.flush();
+//                        }
+//                        writer.write("<eof>\n");
+//                        writer.flush();
+//                    }
+//                };
+//
+//
+//                send.start();
+
+
                 socket.close();
                 System.out.println("Disconnected...");
             }
