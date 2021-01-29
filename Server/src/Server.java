0aac84d8420ca822320d47687406bfbb464c29ee
Rajeet Hossain <rhossain192096@bscse.uiu.ac.bd>
2021-01-29 16:03:04 +0600

    Server Forth Commit

--- Server/src/Server.java
+++ Server/src/Server.java
@@ -14,9 +14,6 @@ public class Server{
 
             File temp = new File("temp.txt");
 
-
-
-
             while (true) {
                 Socket socket = server.accept();
 
@@ -32,106 +29,6 @@ public class Server{
                 Scanner fileReader = new Scanner(otdt.otdt);
                 FileWriter fileWriter = new FileWriter(temp);
 
-
-
-
-//                try {
-//                    while (reader.hasNextLine()) {
-//                        String line = reader.nextLine();
-//                        if (line.equals("<eof>")) {
-//                            otdt.updateFile(temp);
-//                            break;
-//                        }
-//                        System.out.println(line);
-//                        fileWriter.append(line + "\n");
-//                        fileWriter.flush();
-//                    }
-//                }
-//                catch (Exception e){
-//                    System.out.println(e.getMessage());
-//                }
-
-//
-//                while (fileReader.hasNextLine()){
-//                    String line = fileReader.nextLine();
-//                    writer.write(line + "\n");
-//                    writer.flush();
-//                }
-//                writer.write("<eof>\n");
-//                writer.flush();
-//
-//                String source = reader.nextLine();
-//
-//                Thread receive = new Thread(){
-//                    @Override
-//                    public void run() {
-//                        if(source.equals("WEMS")) {
-//                            try {
-//                                while (reader.hasNextLine()) {
-//                                    String line = reader.nextLine();
-//                                    if (line.equals("<eof>")) {
-//                                        otdt.updateFileE(temp1);
-//                                        break;
-//                                    }
-//                                    System.out.println(line);
-//                                    fileWriterE.append(line + "\n");
-//                                    fileWriterE.flush();
-//                                }
-//                            } catch (Exception e) {
-//                                System.out.println(e.getMessage());
-//                            }
-//                        }
-//
-//                        if(source.equals("WOTDT")) {
-//                            try {
-//                                while (reader.hasNextLine()) {
-//                                    String line = reader.nextLine();
-//                                    if (line.equals("<eof>")) {
-//                                        otdt.updateFileE(temp2);
-//                                        break;
-//                                    }
-//                                    System.out.println(line);
-//                                    fileWriterO.append(line + "\n");
-//                                    fileWriterO.flush();
-//                                }
-//                            } catch (Exception e) {
-//                                System.out.println(e.getMessage());
-//                            }
-//                        }
-//
-//
-//                    }
-//                };
-//                receive.start();
-//
-//
-//                Thread send = new Thread(){
-//                    @Override
-//                    public void run() {
-//                        if(source.equals("REMS")) {
-//                            while (fileReaderO.hasNextLine()) {
-//                                String line = fileReaderO.nextLine();
-//                                writer.write(line + "\n");
-//                                writer.flush();
-//                            }
-//                            writer.write("<eof>\n");
-//                            writer.flush();
-//                        }
-//
-//                        if(source.equals("ROTDT")) {
-//                            while (fileReaderE.hasNextLine()) {
-//                                String line = fileReaderE.nextLine();
-//                                writer.write(line + "\n");
-//                                writer.flush();
-//                            }
-//                            writer.write("<eof>\n");
-//                            writer.flush();
-//                        }
-//                    }
-//                };
-//                send.start();
-//
-
                 Thread receive = new Thread(){
                     @Override
                     public void run() {
@@ -146,6 +43,7 @@ public class Server{
                                 fileWriter.append(line + "\n");
                                 fileWriter.flush();
                             }
+                            System.out.println("Disconnected...");
                         }
                         catch (Exception e){
                             System.out.println(e.getMessage());
@@ -165,6 +63,7 @@ public class Server{
                         }
                         writer.write("<eof>\n");
                         writer.flush();
+                        System.out.println("Disconnected...");
                     }
                 };
                 send.start();
@@ -200,22 +99,4 @@ class OTDT{
             System.out.println(e.getMessage());
         }
     }
-//    public void updateFileO(File file) {
-//        try {
-//            Scanner scn = new Scanner(file);
-//            FileWriter updateData = new FileWriter(otdt);
-//
-//            while (scn.hasNextLine()) {
-//                updateData.write(scn.nextLine() + "\n");
-//            }
-//
-//            updateData.close();
-//            scn.close();
-//
-//            file.delete();
-//
-//        } catch (IOException e) {
-//            System.out.println(e.getMessage());
-//        }
-//    }
 }
